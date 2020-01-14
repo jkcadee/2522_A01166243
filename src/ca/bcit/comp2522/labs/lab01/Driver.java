04a5196900fbc67a5686ebad902bc2b00c21dc2d
Jkcadee <janellesertyankwok@gmail.com>
2020-01-13 23:31:12 -0800

    created simulateRaces method in Driver

--- src/ca/bcit/comp2522/labs/lab01/Driver.java
+++ src/ca/bcit/comp2522/labs/lab01/Driver.java
@@ -1,10 +1,14 @@
 package ca.bcit.comp2522.labs.lab01;
 
 public class Driver {
+
+    public static String simulateRaces(int numOfRaces, int lengthOfRace) {
+        return String.format("The number of races was %d and the length of the race was %d.", numOfRaces, lengthOfRace);
+    }
+
     public static void main(String[] args) {
-        Race race = new Race(100);
-        for (int i = 0; i < 100; i++) {
-            System.out.println(race.simulateRace());
-        }
+        Race race100units = new Race(100);
+        Race race1000units = new Race(1000);
+
     }
 }
